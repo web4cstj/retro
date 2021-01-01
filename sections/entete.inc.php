c710c03bf5ca03df7d9bda835a8ad7254550d7b3
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-12-31 19:01:43 -0500

    Départ

--- sections/entete.inc.php
+++ sections/entete.inc.php
@@ -1,9 +1,1 @@
-<!--DEBUT HAUT-->
-<div id="top">
-	<div class="topright">
-		<?php include("sections/titre.inc.php"); ?>
-		<?php include("sections/pub.inc.php"); ?>
-	</div>
-  <?php include("sections/logo.inc.php"); ?>
-</div>
-<!--FIN HAUT-->
+
